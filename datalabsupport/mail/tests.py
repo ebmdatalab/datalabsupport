9766fcabf59ae4702026862fc63dc9f9b803f003
Seb Bacon <seb.bacon@gmail.com>
2018-11-07 11:53:29 +0000

    Fix incorrect test

--- datalabsupport/mail/tests.py
+++ datalabsupport/mail/tests.py
@@ -55,7 +55,7 @@ class TestEmailParsing(TestCase):
 
         get_messages(folder='INBOX', channel='#mailtest')
         expected = ("<http://info.sagepub.co.uk/c/"
-                    "11o8zNX6zKkZTZfhD08iisMdxM3A|_blank|submission "
+                    "11o8zNX6zKkZTZfhD08iisMdxM3A|submission "
                     "guidelines>")
         returned = mock_slack.return_value.api_call.call_args[-1]['text']
         self.assertIn(expected, returned)
